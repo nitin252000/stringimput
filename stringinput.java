bf679ddefe183453690c11f93bf5d106902f3e41
nitin252000 <68811769+nitin252000@users.noreply.github.com>
2020-11-08 19:12:06 +0530

    Add files via upload

---
+++ stringinput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class stringinput {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your string");
+        String s=sc.next();
+        System.out.println("your input string is"+"->"+s);
+    }
+}
